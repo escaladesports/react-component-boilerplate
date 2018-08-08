48850350fe383cf975cf8e9d74a60238cbd54259
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-08 10:53:20 -0500

    ESLint

--- src/module/index.js
+++ src/module/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default class ExampleComponent extends React.Component {
 	static defaultProps = {
-		name: `Component`
+		name: `Component`,
 	}
 	render() {
 		const props = Object.assign({}, this.props)
